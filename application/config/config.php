b1737eb1cd579ca6ac7344c2745b784aa53f479e
Ashish Raj <rajjashish1500@gmail.com>
2025-01-25 12:35:46 +0530

    path changes

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ $config['base_url']	= 'http://localhost/hotel_management_system';
 |
 */
 $config['index_page'] = '';
-$config['base_url'] = 'http://localhost/hms/hotel_management_system/';
+$config['base_url'] = 'http://localhost/hms/hms/hotel_management_system/';
 
 
 /*
---
+++ application/logs/log-2025-01-25.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-01-25 08:03:06 --> 404 Page Not Found: Logi/index
